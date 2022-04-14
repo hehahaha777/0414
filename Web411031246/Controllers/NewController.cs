3f167c3689327a5f5b214d8446fe411e44e22b00
贝克帆 <puikefarn@gmail.com>
2022-04-14 20:48:44 +0800

    new push

---
+++ Web411031246/Controllers/NewController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web411031246.Controllers
+{
+    public class NewController : Controller
+    {
+        // GET: New
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult HW1()
+        {
+            return View();
+        }
+    }
+}
