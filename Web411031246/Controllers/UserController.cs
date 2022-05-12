6cc91281738bf9bc304dbba18590c47fe8cf6fc0
puikefarn <wochaoniba1234@gmail.com>
2022-05-12 20:52:48 +0800

    新增加post？

---
+++ Web411031246/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web411031246.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(string name,string account,string password)
+        {
+            if (name.IsNullOrWhiteSpace())
+            {
+                ViewBag.NameMessage = "请输入名字";
+            }
+            if (account.IsNullOrWhiteSpace())
+            {
+                ViewBag.accountMessage = "请输入账号";
+
+            }
+            if (password.IsNullOrWhiteSpace())
+            {
+                ViewBag.passwordMessage = "请输入密码";
+
+            }
+            if (!name.IsNullOrWhiteSpace() && !account.IsNullOrWhiteSpace() && !password.IsNullOrWhiteSpace())
+            {
+                ViewBag.Message = "注册成功";
+            }
+            return View();
+            
+        }
+        
+        
+    }
+}
